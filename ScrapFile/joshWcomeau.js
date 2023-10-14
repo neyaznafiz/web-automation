ec1edc76489e299c97e74458f2611ea3c1e46050
NEYAZ NAFIZ <neyaz@aintron.com>
2023-10-15 01:44:53 +0600

    another scrap file

---
+++ ScrapFile/joshWcomeau.js
@@ -0,0 +1,32 @@
+import puppeteer from "puppeteer";
+
+const url = "https://www.joshwcomeau.com/";
+
+const joshWcomeau = async () => {
+  const browser = await puppeteer.launch({
+    headless: "new",
+    ignoreDefaultArgs: ["--disable-extensions"],
+  });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const allArticles = await page.evaluate(() => {
+    const articles = document.querySelectorAll("article");
+
+    const content = Array.from(articles).slice(0, 5).map((article) => {
+        const title = article.querySelector("h3").innerText;
+        const link = article.querySelector("a");
+        const url = link.href;
+        const paragraph = link.querySelector("p").innerText;
+
+        return { title, url, paragraph };
+      });
+
+    return content;
+  });
+
+  console.log(allArticles);
+  await browser.close();
+};
+
+export {joshWcomeau}
--- index.js
+++ index.js
@@ -1,32 +1,10 @@
 import puppeteer from "puppeteer";
-
-const url = "https://www.joshwcomeau.com/";
+import { joshWcomeau } from "./ScrapFile/joshWcomeau.js";
+import { overReacted } from "./ScrapFile/overReacted.js";
 
 const main = async () => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    ignoreDefaultArgs: ["--disable-extensions"],
-  });
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  const allArticles = await page.evaluate(() => {
-    const articles = document.querySelectorAll("article");
-
-    const content = Array.from(articles).slice(0, 5).map((article) => {
-        const title = article.querySelector("h3").innerText;
-        const link = article.querySelector("a");
-        const url = link.href;
-        const paragraph = link.querySelector("p").innerText;
-
-        return { title, url, paragraph };
-      });
-
-    return content;
-  });
-
-  console.log(allArticles);
-  await browser.close();
+  joshWcomeau()
+  overReacted()
 };
 
 main();
