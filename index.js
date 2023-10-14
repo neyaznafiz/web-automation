bb6b2fa46da7364abf7cc6204b3dd50920e7ed76
NEYAZ NAFIZ <neyaz@aintron.com>
2023-10-14 23:21:41 +0600

    take screenshot

---
+++ index.js
@@ -0,0 +1,16 @@
+import puppeteer from "puppeteer";
+
+const url = "https://www.joshwcomeau.com/";
+
+const main = async () => {
+  const browser = await puppeteer.launch({
+    headless: "new",
+    ignoreDefaultArgs: ['--disable-extensions'],
+});
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.screenshot({ path: "web-scraping.png" });
+  await browser.close();
+};
+
+main();
