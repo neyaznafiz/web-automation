06bf678278707dfff0dc167f982373d3e8e7d0da
NEYAZ NAFIZ <neyaz@aintron.com>
2023-10-15 00:01:39 +0600

    scraping content

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ const url = "https://www.joshwcomeau.com/";
 const main = async () => {
   const browser = await puppeteer.launch({
     headless: "new",
-    ignoreDefaultArgs: ['--disable-extensions'],
-});
+    ignoreDefaultArgs: ["--disable-extensions"],
+  });
   const page = await browser.newPage();
   await page.goto(url);
-  await page.screenshot({ path: "web-scraping.png" });
+
+  const allArticles = await page.evaluate(() => {
+    const articles = document.querySelectorAll("article");
+
+    const content = Array.from(articles).slice(0, 5).map((article) => {
+        const title = article.querySelector("h3").innerText;
+        const url = article.querySelector("a").href;
+
+        return { title, url };
+      });
+
+    return content;
+  });
+
+  console.log(allArticles);
   await browser.close();
 };
 
