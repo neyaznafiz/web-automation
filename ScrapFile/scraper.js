21750bcd9cda4a29de450e42b3c9eb06fb0e363a
NEYAZ NAFIZ <neyaz@aintron.com>
2023-10-18 14:04:27 +0600

    scrap done

--- ScrapFile/scraper.js
+++ ScrapFile/scraper.js
@@ -9,19 +9,21 @@ const scraper = async (url, elements, contentElement) => {
   await page.goto(url);
 
   const allCategories = await page.evaluate((allElementSelector, contentSelector) => {
-
-    const categories = document.querySelectorAll(allElementSelector);
-    const content = Array.from(categories).map((category) => {
+      const categories = document.querySelectorAll(allElementSelector);
+      const content = Array.from(categories).map((category) => {
         const result = category.querySelector(contentSelector).innerText;
 
         return { result };
       });
 
-    return content;
-  }, elements, contentElement);
+      return content;
+    },
+    elements,
+    contentElement
+  );
 
   console.log(allCategories);
   await browser.close();
 };
 
-export {scraper}
+export { scraper };
